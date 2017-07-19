252d6002a7dad609ce9298603c0252ca0837afe3
Christian Freitas <chrfreitas@gmail.com>
2017-07-19 19:59:28 -0300

    Added CounterStore

--- src/js/stores/CounterStore.js
+++ src/js/stores/CounterStore.js
@@ -1,13 +1,45 @@
 'use strict';
 
 import AppDispatcher from '../dispatcher/AppDispatcher';
-import CounterContsnats from '../constants/CounterConstants.js';
+import CounterConstants from '../constants/CounterConstants.js';
 import Events from 'events';
 
 const EventEmitter = Events.EventEmitter;
 
 class CounterStore extends EventEmitter {
-    constructor() {
 
+    emitChange(){
+        this.emit('change');
     }
+
+    addChangeListener(callback){
+        this.on('change', callback)
+    }
+
+    remoceChangeListener(callback){
+        this.removeListener('change', callback);
+    }
+
 }
+
+let counterStore = new CounterStore();
+
+AppDispatcher.register((payload){
+
+    let action = payload.action;
+
+    switch (action.actionType) {
+        case CounterStore.ADD:
+            console.log('Add');
+            break;
+
+        case CounterStore.REMOVE:
+            console.log('Remove');
+            break;
+    }
+
+    counterStore.emitChange();
+
+});
+
+exports default counterStore;
