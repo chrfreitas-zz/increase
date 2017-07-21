4487682feae372652f96d190454988a84dd8c011
Christian Freitas <chrfreitas@gmail.com>
2017-07-21 20:19:43 -0300

    Fix dispatch called many times

--- src/js/dispatcher/app.dispatcher.js
+++ src/js/dispatcher/app.dispatcher.js
@@ -6,6 +6,11 @@ const SOURCE = 'VIEW_ACTION';
 let AppDispatcher = new Flux.Dispatcher();
 
 AppDispatcher.viewAction = function(action = {}){
+
+    if(this.isDispatching()){
+        return;
+    }
+
     this.dispatch({
         source: SOURCE,
         action,
