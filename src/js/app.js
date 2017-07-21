212f40e8c2234b551ea5573b8eda563de2517c49
Christian Freitas <chrfreitas@gmail.com>
2017-07-21 19:58:32 -0300

    Add style base

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './components/counter.component';
+import Style from '../style/app.scss';
 
 ReactDOM.render(<Counter />, document.getElementById('main'));
