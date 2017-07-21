b40507de136145774f6299fdd6a7242e56ffb049
Christian Freitas <chrfreitas@gmail.com>
2017-07-21 20:20:42 -0300

    Added event listener from Background to change

--- src/js/components/background.component.js
+++ src/js/components/background.component.js
@@ -1,8 +1,19 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './counter.component';
+import CounterStore from '../stores/counter.store';
 
 class Background extends React.Component {
+    componentDidMount(){        
+        CounterStore.addChangeListener(this.onChange);
+    }
+
+    onChange(){
+        console.log('Change');
+    }
+
     render() {
         return (
             <div>
